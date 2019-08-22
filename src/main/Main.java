6b10a0793f51b085d72ccb4d484a466ff73d5483
talat <talat.misbah_cs18@gla.ac.in>
2019-08-22 02:32:48 -0700

    create main method

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,8 @@
 package main;
 /**/
 
-import java.util.Scanner;
-
 public class Main {
-    public public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+    public static void main(String[] args) {
 
 
     }
