ca2730e22cccbd641aba3c1b66353a4c7553437e
talat <talat.misbah_cs18@gla.ac.in>
2019-08-22 02:46:39 -0700

    input done

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner:(System.in);
         String userName = scanner.nextLine().trim();
+        System.out.println("Welcome," + userName + "!");
         Scanner.close();
 
 
